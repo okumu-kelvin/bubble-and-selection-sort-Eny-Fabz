bf445296e7457755d3078bd5777b6010a3964e5d
Eniola Faithfulness Fabunmi <eniola.fabunmi@strathmore.edu>
2025-06-20 13:46:28 +0300

    Submission of bubble and selection sort assignment

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,16 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+import random
+def bubble_sort(mine):
+    x = len(mine)
+    for i in range(x):
+        for j in range(0, x - i - 1):
+            if mine[j] > mine[j + 1]:
+                mine[j], mine[j + 1] = mine[j + 1], mine[j]
+    return mine
+
+if __name__ == '__main__':
+    my_list = [i for i in range(1,25,2)]
+    random.shuffle(my_list)
+
+    print("My original shuffled list:", my_list)
+    b_sorted_list = bubble_sort(my_list.copy())
+    print("Sorted list:", b_sorted_list)
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,18 @@
-def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+import random
+def selection_sort(yours):
+    x = len(yours)
+    for i in range(x):
+        min_index = i
+        for j in range(i+1, x):
+            if yours[j] < yours[min_index]:
+                min_index = j
+        yours[i], yours[min_index] = yours[min_index], yours[i]
+    return yours
+
+if __name__ == '__main__':
+    my_list = [i for i in range(1,25,2)]
+    random.shuffle(my_list)
+
+    print("My original shuffled list:", my_list)
+    s_sorted_list = selection_sort(my_list.copy())
+    print("Sorted list:", s_sorted_list)
